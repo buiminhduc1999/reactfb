999ef27a27374c47c1887244e2d5eda2b0aa5729
chienvu123 <chienvu1308@gmail.com>
2020-07-07 23:05:44 +0700

    demo router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component, useCallback } from 'react';
 
 import HomePage from './pages/customer/HomePage';
 import { Switch, Route } from 'react-router-dom'
 import Login from './screens/LoginScreen';
 import Logout from './screens/LoginScreen/Logout';
+import appRouters from './routers';
 
 function App() {
+  const renderRouter = useCallback(() =>
+  appRouters.map((router, index) => {
+      return (
+        <Route
+          key={index.toString()}
+          path={`${router.parentPath}${router.path}`}
+          component={router.component}
+          exact={router.exact === true}
+        />
+      );
+   
+  }), []);
   return (
     <Switch>
-      <Route exact path="/" component={Login}></Route>
-      <Route path="/home" component={HomePage}></Route>
-      <Route path="/logout" component={Logout}></Route>
-
+      {renderRouter()}
     </Switch>
   );
 }
